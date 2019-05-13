12442c218708587104b8fff0084f9c635fe99e9e
Bryan <sanbryan11@gmail.com>
2019-05-13 16:39:40 -0500

    Personalización de botones

--- src/Vista/Matriz.java
+++ src/Vista/Matriz.java
@@ -11,6 +11,9 @@ public class Matriz extends JPanel{
       for (int contY = 0; contY < y; contY++){
         JButton btn = new JButton();
         btn.setSize(x,y);
+        ImageIcon cesped = new ImageIcon("cesped.jpg");
+        btn.setIcon(cesped);
+        btn.setIcon(new ImageIcon(new ImageIcon("cesped.jpg").getImage().getScaledInstance(btn.getWidth(),btn.getHeight(),Image.SCALE_DEFAULT)));
         MatrizBotones[contX][contY] = btn;
         add(MatrizBotones[contX][contY]);
       }
